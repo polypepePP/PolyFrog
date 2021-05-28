3643cc2210709c929b047618eb1bddbcfc98020a
PolyFrogFarm <syfyuser2@gmail.com>
2021-05-28 17:08:25 -0400

    ypp.sol

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 module.exports = router;
 
-// router.get('/', (__, res) => {
-//     res.sendFile(path.join(__dirname, '/../public/html/index.html'));
+// router.get('/2', (__, res) => {
+//     res.sendFile(path.join(__dirname, '/../public/html/farm2.html'));
 // });
 
 router.get('/', (__, res) => {
